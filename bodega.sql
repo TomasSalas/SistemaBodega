e1862eca12caa1cd59d575e7ae44cbc7594cb2c8
TomasSalas <tsalasarancibia@gmail.com>
2022-05-05 17:01:13 -0400

    OK

--- bodega.sql
+++ bodega.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-05-2022 a las 22:32:29
+-- Tiempo de generación: 05-05-2022 a las 23:00:42
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -31,53 +31,28 @@ CREATE TABLE `detalle_venta` (
   `id_detalle_venta` varchar(250) NOT NULL,
   `id_venta` varchar(250) NOT NULL,
   `id_producto` varchar(250) NOT NULL,
-  `precio_venta` int(11) NOT NULL
+  `precio_venta` decimal(20,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfil_usuario`
+--
+
+CREATE TABLE `perfil_usuario` (
+  `id_perfil` int(2) NOT NULL,
+  `descripcion` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `detalle_venta`
+-- Volcado de datos para la tabla `perfil_usuario`
 --
 
-INSERT INTO `detalle_venta` (`id_detalle_venta`, `id_venta`, `id_producto`, `precio_venta`) VALUES
-('', '12893427754', '7801505001737', 2990),
-('', '12893427754', '7801505001737', 2990),
-('', '12893427754', '', 0),
-('', '12893427754', '7801620005160', 1500),
-('', '964072363419', '7801505001737', 2990),
-('', '964072363419', '', 0),
-('', '294593115064', '1234', 30000),
-('', '294593115064', '123456', 45000),
-('', '218092541626', '1234', 30000),
-('', '218092541626', '123456', 45000),
-('', '370957999543', '123456', 45000),
-('', '370957999543', '1234', 30000),
-('', '19995712208', '1234', 30000),
-('', '19995712208', '1234', 30000),
-('', '19995712208', '1234', 30000),
-('', '906849447689', '1234', 30000),
-('', '906849447689', '', 0),
-('', '906849447689', '', 0),
-('', '906849447689', '1234', 30000),
-('', '881744379026', '1234', 30000),
-('', '881744379026', '', 0),
-('', '881744379026', '', 0),
-('', '881744379026', '', 0),
-('', '881744379026', '1234', 30000),
-('', '559295339136', '1234', 30000),
-('', '559295339136', '123456', 45000),
-('', '986091196401', '1234', 30000),
-('', '986091196401', '1234', 30000),
-('', '986091196401', '1234', 30000),
-('', '986091196401', '1234', 30000),
-('', '986091196401', '1234', 30000),
-('', '986091196401', '1234', 30000),
-('', '986091196401', '1234', 30000),
-('', '243287373408', '1234', 30000),
-('', '243287373408', '1234', 30000),
-('', '297170993932', '1234', 30000),
-('', '883186649550', '1234', 30000),
-('', '956990065315', '1234', 30000),
-('', '874991977976', '1234', 30000);
+INSERT INTO `perfil_usuario` (`id_perfil`, `descripcion`) VALUES
+(1, 'Administrador'),
+(2, 'Bodega'),
+(3, 'Venta');
 
 -- --------------------------------------------------------
 
@@ -100,10 +75,14 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`codigo`, `nom_producto`, `cant_producto`, `precio_compra`, `precio_venta`, `comen_producto`, `estado`) VALUES
-('1234', 'Pantalla', 8, 15000, 30000, 'Pantalla', 1),
+('', '', 0, 0, 0, '', 0),
+('123', '', 0, 0, 0, '', 0),
+('1234', 'Pantalla Xiaomi POCO X3', 0, 15000, 30000, 'Pantalla', 1),
 ('123456', 'Bateria Iphone 11 Pro MAX', 20, 15000, 45000, 'Bateria Alternativa AAA', 1),
 ('7801505001737', 'Endulzante', 50, 1500, 2990, 'Endulzante Iansa', 1),
-('7801620005160', 'Gatorade Azul', 50, 800, 1500, 'Liquido Azul', 1);
+('7801610000571', 'Coca Cola 591 ML', 10, 850, 1290, 'Coca Medio', 1),
+('7801620005160', 'Gatorade Azul', 50, 800, 1500, 'Liquido Azul', 0),
+('7801620007485', 'Ken Piña 1.5L', 20, 1500, 1990, 'Ken Light', 1);
 
 -- --------------------------------------------------------
 
@@ -133,15 +112,16 @@ INSERT INTO `tipo_pago` (`id_tipo_pago`, `detalle_pago`) VALUES
 
 CREATE TABLE `usuarios` (
   `usuario` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL
+  `pass` varchar(250) NOT NULL,
+  `id_perfil` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`usuario`, `password`) VALUES
-('tomas', '2bc6038c3dfca09b2da23c8b6da8ba884dc2dcc2');
+INSERT INTO `usuarios` (`usuario`, `pass`, `id_perfil`) VALUES
+('19448019-9', 'ce9156ae19aa2c61f31509ea48f81ca9', 1);
 
 -- --------------------------------------------------------
 
@@ -153,20 +133,25 @@ CREATE TABLE `venta` (
   `id_venta` varchar(250) NOT NULL,
   `fecha_venta` date NOT NULL,
   `id_tipo_pago` int(2) NOT NULL,
-  `total_venta` int(20) NOT NULL
+  `total_venta` decimal(20,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `venta`
---
-
-INSERT INTO `venta` (`id_venta`, `fecha_venta`, `id_tipo_pago`, `total_venta`) VALUES
-('227329514960', '2022-05-03', 1, 0),
-('883186649550', '2022-05-03', 1, 30000);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD PRIMARY KEY (`id_detalle_venta`),
+  ADD KEY `id_venta` (`id_venta`);
+
+--
+-- Indices de la tabla `perfil_usuario`
+--
+ALTER TABLE `perfil_usuario`
+  ADD PRIMARY KEY (`id_perfil`);
 
 --
 -- Indices de la tabla `productos`
@@ -181,6 +166,13 @@ ALTER TABLE `tipo_pago`
   ADD PRIMARY KEY (`id_tipo_pago`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`usuario`),
+  ADD KEY `id_perfil` (`id_perfil`);
+
+--
 -- Indices de la tabla `venta`
 --
 ALTER TABLE `venta`
@@ -190,6 +182,18 @@ ALTER TABLE `venta`
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD CONSTRAINT `detalle_venta_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_perfil`) REFERENCES `perfil_usuario` (`id_perfil`);
 
 --
 -- Filtros para la tabla `venta`
